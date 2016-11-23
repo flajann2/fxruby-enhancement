0d02ecd5cbe970e0ddb4bf492753f75d500ebba4
Fred Mitchell <fred.mitchell@gmx.de>
2016-11-23 16:45:40 +0100

    refactored a bit

--- lib/fxruby-enhancement.rb
+++ lib/fxruby-enhancement.rb
@@ -4,18 +4,41 @@ module Fox
   
   # include this in your top class objects.
   module Enhancement
+    def self.included(klass)
+      klass.extend ClassMethods
+    end
+
+    # Module-level
     class << self
-      attr_accessor :app
+      attr_accessor :application
       
       def app_set name, vendor
-        @app = FXApp.new(name, vendor)
-      end
-
-      def run
-        @app.run
+        @application = FXApp.new(name, vendor)
       end
       
+      def app_activate
+        @application.create
+        @application.run
+      end
     end
-  end
-  
+
+    # class level
+    module ClassMethods
+      
+
+    end
+
+    # instance level
+    def app_set name, vendor
+      Enhancement.app_set name, vendor
+    end
+
+    def app_activate
+      Enhancement.app_activate
+    end
+
+    def application
+      Enhancement.application
+    end
+  end 
 end
