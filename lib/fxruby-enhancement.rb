57ad01c0bd4fc0bd083456c3546fb1af4ff6a41f
Fred Mitchell <fred.mitchell@gmx.de>
2016-11-24 10:30:01 +0100

    working with tricky class instance vairables

--- lib/fxruby-enhancement.rb
+++ lib/fxruby-enhancement.rb
@@ -1,11 +1,24 @@
+# coding: utf-8
 require 'fox16'
 
 module Fox
   
   # include this in your top class objects.
+  # If for a module, you want to extend, rather
+  # than include FIXME: later we will clean
+  # this up.
   module Enhancement
     def self.included(klass)
       klass.extend ClassMethods
+      klass.class_eval do
+        def initialize
+          require 'pry'; binding.pry #DEBUGGING
+          super application,
+                self.class.app_title,
+                width: self.class.win_width,
+                height: self.class.win_height
+        end
+      end
     end
 
     # Module-level
@@ -24,11 +37,37 @@ module Fox
 
     # class level
     module ClassMethods
-      
+      def window &block
+        
+        def title t
+          @app_title = t
+        end
+        
+        def width w
+          @win_width = w
+        end
+        
+        def height h
+          @win_height = h
+        end
 
+        def show s
+          @win_show = s
+        end
+        
+        instance_eval &block 
+      end
+      
+      # TODO: Metaprogramming to dry this up!!!
+      def app_title;  instance_variable_get :@app_title;  end
+      def win_width;  instance_variable_get :@win_width;  end
+      def win_height; instance_variable_get :@win_height; end
+      def win_show;   instance_variable_get :@win_show;   end
     end
+    
 
     # instance level
+   
     def app_set name, vendor
       Enhancement.app_set name, vendor
     end
@@ -40,5 +79,10 @@ module Fox
     def application
       Enhancement.application
     end
+    
+    def create
+      super
+      show self.class.win_show
+    end   
   end 
 end
