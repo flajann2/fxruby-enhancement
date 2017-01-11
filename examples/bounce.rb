67cf503b2a7a3e6251be86c7ea7a2b00bd1cf1a5
Fred Mitchell <fred.mitchell@gmx.de>
2017-01-11 07:43:27 +0100

    examples in the making

--- examples/bounce.rb
+++ examples/bounce.rb
@@ -1,2 +1,155 @@
 require 'fxruby-enhancement'
 
+include Fox
+include Fox::Enhancement::Mapper
+
+class Ball
+  attr_reader :color
+  attr_reader :center
+  attr_reader :radius
+  attr_reader :dir
+  attr_reader :x, :y
+  attr_reader :w, :h
+  attr_accessor :worldWidth
+  attr_accessor :worldHeight
+
+  
+  def initialize r
+    @radius = r
+    @w = 2*@radius
+    @h = 2*@radius
+    @center = FXPoint.new(50, 50)
+    @x = @center.x - @radius
+    @y = @center.y - @radius
+    @color = FXRGB(255, 0, 0) # red
+    @dir = FXPoint.new(-1, -1)
+    setWorldSize(1000, 1000)
+  end
+  
+  # Draw the ball into this device context
+  def draw(dc)
+    dc.setForeground(color)
+    dc.fillArc(x, y, w, h, 0, 64*90)
+    dc.fillArc(x, y, w, h, 64*90, 64*180)
+    dc.fillArc(x, y, w, h, 64*180, 64*270)
+    dc.fillArc(x, y, w, h, 64*270, 64*360)
+  end
+
+  def bounce_x
+    @dir.x=-@dir.x
+  end
+
+  def bounce_y
+    @dir.y=-@dir.y
+  end
+
+  def collision_y?
+    (y<0 && dir.y<0) || (y+h>worldHeight && dir.y>0)
+  end
+
+  def collision_x?
+    (x<0 && dir.x<0) || (x+w>worldWidth && dir.x>0)
+  end
+
+  def setWorldSize(ww, wh)
+    @worldWidth = ww
+    @worldHeight = wh
+  end
+  
+  def move(units)
+    dx = dir.x*units
+    dy = dir.y*units
+    center.x += dx
+    center.y += dy
+    @x += dx
+    @y += dy
+    if collision_x?
+      bounce_x
+      move(units)
+    end
+    if collision_y?
+      bounce_y
+      move(units)
+    end
+  end
+end
+
+fx_app :app do
+  app_name "Bounce"
+  vendor_name "Example"
+
+  fx_image :back_buffer {
+    opts IMAGE_KEEP
+  }
+  
+  fx_main_window :bounce_window {
+    title "Ball Demo"
+    opts DECOR_ALL
+    width 400
+    height 300
+    
+    $ball = Ball.new(20)
+
+    fx_canvas :canvas {
+      opts LAYOUT_FILL_X|LAYOUT_FILL_Y
+      
+      instance { |c|
+        c.sel_paint { |sender, sel, event|
+          FXDCWindow.new(sender, event) { |dc|
+            dc.drawImage(ref(:back_buffer), 0, 0)
+          }
+        }
+
+        c.sel_configure{ |sender, sel, event|
+          bb = ref(:back_buffer)
+          bb.create unless bb.created?
+          bb.resize(sender.width, sender.height)
+          $ball.setWorldSize(sender.width, sender.height)
+          c.drawScene(bb)
+        }
+
+        def c.drawScene(drawable)
+          FXDCWindow.new(drawable) { |dc|
+            dc.setForeground(FXRGB(255, 255, 255))
+            dc.fillRectangle(0, 0, drawable.width, drawable.height)
+            $ball.draw(dc)
+          }
+        end
+
+        def c.updateCanvas
+          $ball.move(10)
+          drawScene(@backBuffer)
+          @canvas.update
+        end
+
+        #
+        # Handle timeout events
+        #
+        def c.onTimeout(sender, sel, ptr)
+          # Move the ball and re-draw the scene
+          updateCanvas
+          
+          # Re-register the timeout
+          ref(:app).addTimeout(ANIMATION_TIME, method(:onTimeout))
+          
+          # Done
+          return 1
+        end
+      }
+    }
+  }
+end
+
+def run_app
+  fox_component :app do |app|
+    app.create_fox_components
+    app.instance_final_activate
+    app.activate
+    app.run_application
+  end
+end
+
+
+if __FILE__ == $0
+  run_app
+end
