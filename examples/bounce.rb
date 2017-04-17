925f3b79add8634927645fec15f959f56fcfe497
Fred Mitchell <fred.mitchell@gmx.de>
2017-04-17 18:12:27 +0200

    long standing update

--- examples/bounce.rb
+++ examples/bounce.rb
@@ -1,4 +1,7 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+# rubocop:disable all
+
 require 'fxruby-enhancement'
 
 include Fox
@@ -6,6 +9,7 @@ include Fox::Enhancement::Mapper
 
 ANIMATION_TIME = 20
 
+# Handles the "physics" of the ball.
 class Ball
   attr_reader :color
   attr_reader :center
@@ -16,11 +20,10 @@ class Ball
   attr_accessor :worldWidth
   attr_accessor :worldHeight
 
-  
-  def initialize r
+  def initialize(r)
     @radius = r
-    @w = 2*@radius
-    @h = 2*@radius
+    @w = 2 * @radius
+    @h = 2 * @radius
     @center = FXPoint.new(50, 50)
     @x = @center.x - @radius
     @y = @center.y - @radius
@@ -28,40 +31,40 @@ class Ball
     @dir = FXPoint.new(-1, -1)
     setWorldSize(1000, 1000)
   end
-  
+
   # Draw the ball into this device context
   def draw(dc)
     dc.setForeground(color)
-    dc.fillArc(x, y, w, h, 0, 64*90)
-    dc.fillArc(x, y, w, h, 64*90, 64*180)
-    dc.fillArc(x, y, w, h, 64*180, 64*270)
-    dc.fillArc(x, y, w, h, 64*270, 64*360)
+    dc.fillArc(x, y, w, h, 0, 64 * 90)
+    dc.fillArc(x, y, w, h, 64 * 90, 64 * 180)
+    dc.fillArc(x, y, w, h, 64 * 180, 64 * 270)
+    dc.fillArc(x, y, w, h, 64 * 270, 64 * 360)
   end
 
   def bounce_x
-    @dir.x=-@dir.x
+    @dir.x = -@dir.x
   end
 
   def bounce_y
-    @dir.y=-@dir.y
+    @dir.y = -@dir.y
   end
 
   def collision_y?
-    (y<0 && dir.y<0) || (y+h>worldHeight && dir.y>0)
+    (y < 0 && dir.y < 0) || (y + h > worldHeight && dir.y > 0)
   end
 
   def collision_x?
-    (x<0 && dir.x<0) || (x+w>worldWidth && dir.x>0)
+    (x < 0 && dir.x < 0) || (x + w > worldWidth && dir.x > 0)
   end
 
   def setWorldSize(ww, wh)
     @worldWidth = ww
     @worldHeight = wh
   end
-  
+
   def move(units)
-    dx = dir.x*units
-    dy = dir.y*units
+    dx = dir.x * units
+    dy = dir.y * units
     center.x += dx
     center.y += dy
     @x += dx
@@ -78,67 +81,69 @@ class Ball
 end
 
 fx_app :app do
-  app_name "Bounce"
-  vendor_name "Example"
-  
-  fx_main_window(:bounce_window) {
-    title "Bounce Demo"
+  app_name 'Bounce'
+  vendor_name 'Example'
+
+  fx_main_window(:bounce_window) do
+    title 'Bounce Demo'
     opts DECOR_ALL
     width 400
     height 300
 
-    as (:app) {
+    as (:app) do
       fx_image(:back_buffer) { opts IMAGE_KEEP }
-    }
-    
-    instance { |w|
+    end
+
+    instance do |w|
       def w.ball
         @ball ||= Ball.new(20)
       end
-      
+
       def w.drawScene(drawable)
-        FXDCWindow.new(drawable) { |dc|
+        FXDCWindow.new(drawable) do |dc|
           dc.setForeground(FXRGB(255, 255, 255))
           dc.fillRectangle(0, 0, drawable.width, drawable.height)
           ball.draw(dc)
-        }
+        end
       end
-      
+
       def w.updateCanvas
         ball.move(10)
         drawScene(ref(:back_buffer))
         ref(:canvas).update
       end
-      
+
       #
       # Handle timeout events
       #
-      def w.onTimeout(sender, sel, ptr)
+      def w.onTimeout(_sender, _sel, _ptr)
         # Move the ball and re-draw the scene
         updateCanvas
-        
+
         # Re-register the timeout
-        ref(:app).addTimeout(ANIMATION_TIME, ref(:bounce_window).method(:onTimeout))
-        
+        ref(:app)
+          .addTimeout(ANIMATION_TIME,
+                      ref(:bounce_window)
+                        .method(:onTimeout))
         # Done
-        return 1
+        1
       end
-      
+
       w.show PLACEMENT_SCREEN
       ref(:app).addTimeout(ANIMATION_TIME, w.method(:onTimeout))
-    }
-    
-    fx_canvas(:canvas) {
-      opts LAYOUT_FILL_X|LAYOUT_FILL_Y
-      
-      instance { |c|
-        c.sel_paint { |sender, sel, event|
-          FXDCWindow.new(sender, event) { |dc|
-            dc.drawImage(ref(:back_buffer), 0, 0)
-          }
-        }
+    end
 
-        c.sel_configure{ |sender, sel, event|
+    fx_canvas(:canvas) do
+      opts LAYOUT_FILL_X | LAYOUT_FILL_Y
+
+      instance do |c|
+        c.sel_paint do |sender, _sel, event|
+          FXDCWindow.new(sender, event) do |dc|
+            dc.drawImage(ref(:back_buffer), 0, 0)
+          end
+        end
+
+        c.sel_configure do |sender, _sel, _event|
           bb = ref(:back_buffer)
           bb.create unless bb.created?
           bb.resize(sender.width, sender.height)
@@ -146,15 +151,13 @@ fx_app :app do
             bw.ball.setWorldSize(sender.width, sender.height)
             bw.drawScene(bb)
           end
-        }
-      }
-    }
-  }
+        end
+      end
+    end
+  end
 end
 
-if __FILE__ == $0
+if __FILE__ == $PROGRAM_NAME
   # alias for fox_component is fxc
-  fox_component :app do |app|
-    app.launch
-  end
+  fox_component :app, &:launch
 end
